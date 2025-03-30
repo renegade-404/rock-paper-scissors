e29afe934e571c1c6ca0c71e770d29ae5c99d713
renegade-404 <mypcisrenegade@gmail.com>
2025-03-30 13:16:42 +0900

    change final result function logic to if/else if

--- script.js
+++ script.js
@@ -7,44 +7,45 @@ let computerScore = 0;
 
 function playRound() {
 
-    let randomNumber = Math.floor(Math.random()*3);
+    let randomNumber = Math.floor(Math.random() * 3);
     let computerChoice = optionsList[randomNumber];
-    
+
     let humanChoice = prompt("Please choose: rock, paper or scissors!").toLowerCase();
 
     if (humanChoice == "rock" && computerChoice == "scissors"
         || humanChoice == "paper" && computerChoice == "rock"
-        || humanChoice == "scissors" && computerChoice == "paper"
-    ) {
+        || humanChoice == "scissors" && computerChoice == "paper") {
         userScore += 1;
         console.log(`User wins with ${humanChoice}!\nUser score: ${userScore}\nComputer score: ${computerScore}`);
+
     } else if (humanChoice == computerChoice) {
         console.log(`It's a draw with ${humanChoice}s!\nUser score: ${userScore}\nComputer score: ${computerScore}`);
+
     } else {
         computerScore += 1;
         console.log(`Computer wins with ${computerChoice}\nUser score: ${userScore}\nComputer score: ${computerScore}`);
     }
-    
-    
+
+
 }
 
 function finalResults(firstResult, secondResult) {
-    switch (firstResult) {
-        case firstResult < secondResult:
-            console.log(`User loses!\nFinal score:\nUser: ${firstResult}\nComputer: ${secondResult}`);
-        case firstResult > secondResult:
+    if (firstResult < secondResult) {
+        console.log(`User loses!\nFinal score:\nUser: ${firstResult}\nComputer: ${secondResult}`);
+        } else if (firstResult > secondResult) {
             console.log(`User wins!\nFinal score:\nUser: ${firstResult}\nComputer: ${secondResult}`);
-        case firstResult == secondResult:
+        } else {
             console.log(`It's a draw!\nFinal score:\nUser: ${firstResult}\nComputer: ${secondResult}`);
-    }   
-}
+        }
+    }
 
 function playGame() {
     for (let i = 1; i <= 3; i++) {
         console.log(`It's time for round ${i}!`);
         playRound();
-    }
+    };
+
+    finalResults(userScore, computerScore);
 }
 
 playGame();
-// finalResults(userScore, computerScore);
